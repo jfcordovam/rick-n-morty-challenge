26e25e5bd1ebdb994d103232f51df21b30f685ac
Juan Cordova <juan.cordova@cci-entel.cl>
2021-04-24 00:01:01 -0400

    code refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
-const apicalls = require('./apicalls')
 const utils = require('./utils')
 const { performance } = require("perf_hooks");
 
@@ -8,112 +7,59 @@ module.exports = {
       // Measure main execution time
       let mainRunTime = 0;
       performance.mark("main-start")
+      let results = []
 
+      //  For each resource, make the api calls and measure runtime
+      Promise.all([
+         utils.fetchResource('character'),
+         utils.fetchResource('episode'),
+         utils.fetchResource('location')
+      ]).then(([characters, episodes, locations]) => {
 
+         // *** First challenge: character counter ***
+         results.push('First challenge: char counter');
+         let apiRunTime = Math.max(characters.time, episodes.time, locations.time); // Get max time between each of the calls
 
-      //  For each resource, make an api call and measure runtime
-      let [characters, episodes, locations] = await Promise.all([
-         fetchResource('character'),
-         fetchResource('episode'),
-         fetchResource('location')
-      ])
+         results.push(utils.charCounter("l", locations.resource, locations.collection.map(e => e.name)))
+         results.push(utils.charCounter("e", episodes.resource, episodes.collection.map(e => e.name)))
+         results.push(utils.charCounter("c", characters.resource, characters.collection.map(e => e.name)))
 
-      // After each of the api calls (promises) solve, pack into a single results variable and return
-      // values structure -> [[string, array of objects, float], ... ]
-      let results = ['First challenge: char counter'],
-         apiRunTime = 0;
+         performance.mark("main-end")
+         performance.measure("main", "main-start", "main-end")
+         mainRunTime = performance.nodeTiming.duration
 
-      // *** First challenge: character counter ***
-      results.push(firstChallenge("l", locations.map(e => e.name)))
-      results.push(firstChallenge("e", episodes.map(e => e.name)))
-      results.push(firstChallenge("c", characters.map(e => e.name)))
+         results.push(`Execution time duration for first challenge: ${Math.trunc(mainRunTime + apiRunTime)}ms.`) // Add main runtime and api call runtime
 
-      let names = utils.getNames(collection),
-         char = name[0],
-         count = utils.getTotalCharCount(char, names);
+         mainRunTime = 0
+         performance.clearMarks();
 
-      results.push(`The char "${char}" has ${count} ocurrences in ${name} names.`);
-      apiRunTime = (apiRunTime < time) ? time : apiRunTime; // Get max time between each of the calls
+         // *** Second challenge: get episode locations ***
+         performance.mark("main-start")
+         results.push('Second challenge: episode locations')
 
-      performance.mark("main-end")
-      performance.measure("main", "main-start", "main-end")
-      mainRunTime = performance.nodeTiming.duration
+         episodes.collection.forEach(episode => {
 
-      results.push(`Execution time duration for first challenge: ${Math.trunc(mainRunTime + apiRunTime)}ms.`) // Add main runtime and api call runtime
+            let locations = new Set()
 
-      mainRunTime = 0
-      performance.clearMarks();
+            episode.characters.forEach(character => {
 
-      // *** Second challenge: episode locations ***
-      performance.mark("main-start")
-      results.push('Second challenge: episode locations')
+               let characterId = parseInt(character.split('/')[5]);
 
-      let allEpisodes = values[1][1],
-         allCharacters = values[0][1];
+               locations.add(characters.collection.find(c => c.id === characterId).origin.name)
+            });
 
-      allEpisodes.forEach(episode => {
-
-         let locations = new Set()
-
-         episode.characters.forEach(character => {
-
-            let characterId = parseInt(character.split('/')[5]),
-               characterOrigin = utils.getCharacterOrigin(characterId, allCharacters);
-            locations.add(characterOrigin)
+            results.push(`For the characters of episode: "${episode.episode} - ${episode.name}", there are ${locations.size} origins: ${Array.from(locations).join(", ")}.`)
          });
 
-         results.push(`For the characters of episode: "${episode.episode} - ${episode.name}", there are ${locations.size} origins: ${Array.from(locations).join(", ")}.`)
-      });
+         performance.mark("main-end")
+         performance.measure("main", "main-start", "main-end")
+         mainRunTime = performance.nodeTiming.duration
+         performance.clearMarks();
 
-      performance.mark("main-end")
-      performance.measure("main", "main-start", "main-end")
-      mainRunTime = performance.nodeTiming.duration
-      performance.clearMarks();
-
-      results.push(`Execution time duration for second challenge: ${Math.trunc(mainRunTime)}ms.`)
-
+         results.push(`Execution time duration for second challenge: ${Math.trunc(mainRunTime)}ms.`)
+      })
       return results
-
    }
 }
-
-
-let fetchResource = async function (resource) {
-
-   performance.mark(`${resource}-start`) // measure runtime of api calls
-
-   // first get number of pages
-   return apicalls.get(resource).then(response => {
-
-      let pages = response.info.pages,
-         apiPromises = [];
-
-      // then for each page make the rest of the calls as promises 
-      for (let i = 1; i <= pages; i++) {
-         apiPromises.push(apicalls.get(`${resource}?page=${i}`));
-      }
-
-      return Promise.all(apiPromises)
-         .then(responses => {
-
-            let collection = responses.reduce((acc, response) => [...acc, ...response.results], [])
-
-            performance.mark(`${resource}-end`)
-            performance.measure(`${resource}-results`, `${resource}-start`, `${resource}-end`)
-            let runTime = performance.nodeTiming.duration
-
-            return { [resource]: { collection, runTime } } // 'resource' works as identifier 
-         }).then(results => results)
-   })
-}
-
-// "l", locations
-
-function firstChallenge(char, collection) {
-   let charCount = collection.reduce((i, name) =>
-      i + utils.countChars(char, [...name]), 0);
-   return   results.push(`The char "${char}" has ${count} ocurrences in ${name} names.`);
-}
-
 
 
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,22 +1,44 @@
+const { performance } = require("perf_hooks");
+const apicalls = require('./apicalls')
 
 module.exports = {
-   getTotalCharCount(char, collection) {
-      return collection.reduce((i, name) =>
-         i + countChars(char, [...name]), 0);
+   async fetchResource(resource) {
+
+      performance.mark(`${resource}-start`) // measure runtime of api calls
+
+      // first get number of pages
+      return apicalls.get(resource).then(response => {
+
+         let pages = response.info.pages,
+            apiPromises = [];
+
+         // then for each page make the rest of the calls as promises 
+         for (let i = 1; i <= pages; i++) {
+            apiPromises.push(apicalls.get(`${resource}?page=${i}`));
+         }
+
+         return Promise.all(apiPromises)
+            .then(responses => {
+
+               let collection = responses.reduce((acc, response) => [...acc, ...response.results], []) // get all pages results
+
+               performance.mark(`${resource}-end`)
+               performance.measure(`${resource}-results`, `${resource}-start`, `${resource}-end`)
+               let runTime = performance.nodeTiming.duration
+
+               return { 'resource': resource, 'collection': collection, 'time': runTime } // 'resource' works as identifier 
+            }).then(results => results)
+      })
    },
-   getNames(resources) {
-      return resources.map(element => element.name)
-   },
-   getCharacterOrigin(id, characters) {
-      return characters.find(c => c.id === id).origin.name
-   },
-   countChars(char, word) {
-      return word.reduce((i, letter) => {
-         if (char.toLowerCase() === letter.toLowerCase()) {
-            return i + 1
-         } else return i
-      }, 0);
+
+   charCounter(char, resource, collection) {
+      let count = collection.reduce((i, name) => {
+         let re = new RegExp(char, "g");
+         return i + (name.match(re) || []).length
+      }, 0)
+      return `The char "${char}" has ${count} ocurrences in ${resource} names.`;
    }
+
 }
 
 
